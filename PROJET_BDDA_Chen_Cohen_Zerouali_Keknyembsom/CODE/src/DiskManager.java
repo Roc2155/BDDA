37e359840914e0a047d8ec74ad5068e91ab9d9c0
Fazap <113597096+Fazap@users.noreply.github.com>
2022-09-22 14:10:14 +0200

    Update DiskManager.java

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/DiskManager.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/DiskManager.java
@@ -1,17 +1,18 @@
+import java.nio.ByteBuffer;
 
 public class DiskManager {
 	PageId AllocPage () {
-		
+		return null;
 	}
 	
-	void WritePage (PageId pageId, buff) {
+	void WritePage (PageId pageId, ByteBuffer buff) {
 		
 	}
 	void DeallocPage (PageId pageId ) {
 		
 	}
 	int GetCurrentCountAllocPages() {
-		
+		return 0;
 	}
 
 }
