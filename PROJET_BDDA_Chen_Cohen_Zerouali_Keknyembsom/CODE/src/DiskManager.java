24296984dcdc215230399dc3947ed48ec9cfab76
Fazap <113597096+Fazap@users.noreply.github.com>
2022-09-22 13:43:23 +0200

    Ajout du squelette de DiskManager

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/DiskManager.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/DiskManager.java
@@ -1,4 +1,19 @@
 
 public class DiskManager {
+	pageId AllocPage () {
+		
+	}
+	pageId AllocPage (),{
+		
+	}
+	void WritePage (pageId, buff) {
+		
+	}
+	void DeallocPage (pageId) {
+		
+	}
+	int GetCurrentCountAllocPages() {
+		
+	}
 
 }
