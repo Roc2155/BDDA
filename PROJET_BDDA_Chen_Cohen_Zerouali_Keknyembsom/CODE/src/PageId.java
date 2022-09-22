b7b539da3beec534a92702e428905a3f68cfb0d2
Fazap <113597096+Fazap@users.noreply.github.com>
2022-09-22 14:04:36 +0200

    Update PageId.java

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/PageId.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/PageId.java
@@ -1,4 +1,27 @@
 
 public class PageId {
+	private int fileIdx;
+	private int pageIdx;
+	
+	public PageId(int fileIdx, int pageIdx) {
+		this.setFileIdx(fileIdx);
+		this.setPageIdx(pageIdx);
+	}
 
+	public int getFileIdx() {
+		return fileIdx;
+	}
+
+	public void setFileIdx(int fileIdx) {
+		this.fileIdx = fileIdx;
+	}
+
+	public int getPageIdx() {
+		return pageIdx;
+	}
+
+	public void setPageIdx(int pageIdx) {
+		this.pageIdx = pageIdx;
+	}
+	
 }
