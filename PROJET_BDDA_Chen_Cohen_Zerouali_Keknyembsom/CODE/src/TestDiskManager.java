0a714de288a9ebc41c09cb41a97ce256166314fb
Fazap <113597096+Fazap@users.noreply.github.com>
2022-09-26 16:28:02 +0200

    Modification test

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/TestDiskManager.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/TestDiskManager.java
@@ -17,18 +17,13 @@ public class TestDiskManager {
     //PageId pageIdTest = DiskManager.AllocPage(2);
     //System.out.println(pageIdTest.position());
   }
-  public static void TestPageId() {
-	  PageId PageIdTest = new PageId(12, 3);
-	  System.out.println(PageIdTest.getFileIdx());
-	  System.out.println(PageIdTest.getPageIdx());
-  }
+
 
   public static void main(String[] args) {
     //DBParams.DBPath = args[0];
     //DBParams.pageSize = 2;
     //DBParams.maxPagesPerFile = 4;
     TestAllocPage();
-    TestPageId();
-    System.out.println("test compilation");
+    
   }
 }
---
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/TestPageId.java
@@ -0,0 +1,14 @@
+
+public class TestPageId {
+	  public static void TestPageIdMethod() {
+		  PageId PageIdTest = new PageId(12, 3);
+		  System.out.println(PageIdTest.getFileIdx());
+		  System.out.println(PageIdTest.getPageIdx());
+	  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    TestPageIdMethod();
+
+	}
+
+}
