3c44a4f18c64cf944954cc4f0acc0ce9adfed42c
Ouliviaa <oliviachen0815@gmail.com>
2022-09-26 10:23:07 +0200

    TestAllocPage()

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/TestDiskManager.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/TestDiskManager.java
@@ -12,14 +12,16 @@ public class TestDiskManager {
   }
 
   public static void TestAllocPage() {
-    ByteBuffer bufferTest = ByteBuffer.allocate(2);
-    System.out.println(bufferTest.position());
+    //ByteBuffer bufferTest = ByteBuffer.allocate(2);
+    //System.out.println(bufferTest.position());
+    PageId pageIdTest = DiskManager.AllocPage(2);
+    System.out.println(pageIdTest.position());
   }
 
   public static void main(String[] args) {
-    DBParams.DBPath = args[0];
-    DBParams.pageSize = 2;
-    DBParams.maxPagesPerFile = 4;
+    //DBParams.DBPath = args[0];
+    //DBParams.pageSize = 2;
+    //DBParams.maxPagesPerFile = 4;
     TestAllocPage();
   }
 }
