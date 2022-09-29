3a7fe60534b2031a0a981a0fde3717878be80956
Fazap <113597096+Fazap@users.noreply.github.com>
2022-09-29 11:19:23 +0200

    BufferManager

---
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/BufferManager.java
@@ -0,0 +1,46 @@
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+
+public class BufferManager {
+	private static int FrameCount = DBParams.frameCount;
+	DiskManager diskmanager = new DiskManager();
+	
+	private static ArrayList<Frame> ListeDesFrames = new ArrayList<Frame>();
+	public ByteBuffer GetPage(PageId pageid) {
+		if(ListeDesFrames.size()<FrameCount) {//Si on peut rajouter des frames on en rajoute
+			int i;
+			for(Frame frame : ListeDesFrames) {
+				if(frame!=null) {
+					ListeDesFrames.add(frame);
+					i = ListeDesFrames.lastIndexOf(frame);
+				}
+				else {	
+				}
+			}
+			
+			
+			
+			
+			return ListeDesFrames.get(ListeDesFrames.size()-1).getBuff();
+			
+			
+		}
+		else {//Pas de possibilité de rajout de frame, on va donc devoir verifier si elles sont vite, puis eventuellement en liberer
+			
+		}//Faire des comparaisons avec les dirty et pincount dans les frames de la liste des frames
+		
+		
+	}
+	public void FreePage(PageId pageid, boolean valdirty) {
+			
+	}
+	public void FlushAll() {
+		for(Frame frame : ListeDesFrames) {
+			if(frame.isDirty()) {
+				diskmanager.WritePage(frame.get, null);
+			}
+		}
+		
+	}
+
+}
--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Frame.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Frame.java
@@ -1,7 +1,26 @@
+import java.nio.ByteBuffer;
 
 public class Frame {
-	public Frame(PageId pageid, ) {
-		
+	private static ByteBuffer buff;
+	private int pin_count;
+	private boolean dirty;
+	public Frame() {
+		ByteBuffer.allocate(DBParams.pageSize);	
+	}
+	public ByteBuffer getBuff() {
+		return buff;
+	}
+	public int getPin_count() {
+		return pin_count;
+	}
+	public void setPin_count(int pin_count) {
+		this.pin_count = pin_count;
+	}
+	public boolean isDirty() {
+		return dirty;
+	}
+	public void setDirty(boolean dirty) {
+		this.dirty = dirty;
 	}
 
 }
