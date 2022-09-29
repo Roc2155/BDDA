df41dd82bfd21d621fb48300822ee9153a5d4f9f
Fazap <113597096+Fazap@users.noreply.github.com>
2022-09-29 13:29:11 +0200

    4096

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Main.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	  public static void main(String[] args) {
 		    DBParams.DBPath = args[0];
-		    DBParams.pageSize = 2;
+		    DBParams.pageSize = 4096;
 		    DBParams.maxPagesPerFile = 4;
 		    DBParams.frameCount = 2;
 		    TestDiskManager.TestAllocPage();
