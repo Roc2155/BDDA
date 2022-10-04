b834be6ee5af3180d5526d908c5b3194e81355c1
Fazap <113597096+Fazap@users.noreply.github.com>
2022-10-04 21:54:14 +0200

    Bon bufferManager

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/BufferManager.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/BufferManager.java
@@ -1,94 +1,105 @@
+import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
 
 public class BufferManager {
-	private static int FrameCount = DBParams.frameCount;
-	DiskManager diskmanager = DiskManager.getLeDiskManager();
-	public static boolean setFrame = false;
-	private static ArrayList<Frame> ListeDesFrames = new ArrayList<Frame>();
-	private static ArrayList<PageId> ListeDesPages = new ArrayList<PageId>();
+	private static BufferManager INSTANCE;
+	private Frame[] listeDesFrames;
 	private int time;
-	
-	public void setAllFrame() {//A lancer au debut du programme
-		for(int i = 0; i<FrameCount;i++) {
-			ListeDesFrames.add(new Frame());
-			ListeDesPages.add(null);
+	public static BufferManager getInstance() {
+		if(INSTANCE == null) {
+			INSTANCE = new BufferManager();
+			
 		}
-		time=0;
+		
+		return INSTANCE;
 	}
 	
-	
-	public ByteBuffer GetPage(PageId pageid) {
-		ArrayList<PageId> ListeDesPagesVides = new ArrayList<PageId>();
-		ArrayList<PageId> ListeDesPagesPleines = new ArrayList<PageId>();
-		for(PageId pageidlook : ListeDesPages) {
-			if(pageidlook==pageid) {
-				return ListeDesFrames.get(ListeDesPages.indexOf(pageidlook)).getBuff();//Si on trouve la page dans la liste on la retourne directement
-			}
-			else if(pageidlook==null) {
-				ListeDesPagesVides.add(pageidlook);
-			}
-			else {
-				ListeDesPagesPleines.add(pageidlook);
-			}
-			
+	private BufferManager() {
+		listeDesFrames = new Frame[DBParams.frameCount];
+		time =0;
+		for(int i=0;i<DBParams.frameCount;i++) {
+			listeDesFrames[i] = new Frame();
 		}
-		
-		if(!ListeDesPagesVides.isEmpty()) {//Si au moins une casePage vide
-			ListeDesPages.set(ListeDesPages.lastIndexOf(ListeDesPagesVides.get(0)),pageid);//On rajoute pageid a un emplacement precis de la liste
-			int emplacemnet = ListeDesPages.lastIndexOf(pageid);
-			Frame tmpframe = ListeDesFrames.get(emplacemnet);
-			tmpframe.setPin_count(tmpframe.getPin_count()+1);//On incremente le pincount
-			
-			return tmpframe.getBuff();
-		}
-		else {//Si aucunes casesPages vides
-			ArrayList<Frame> ListeDesFramesAPin0 = new ArrayList<Frame>();//Liste des pages a pin 0
-			for(Frame frame : ListeDesFrames) {
-				if(frame.getPin_count()==0) {
-					ListeDesFramesAPin0.add(frame);//on y ajoute toute les frames à pin 0
-				}
-			if(ListeDesFrames.size()==0) {
-				System.out.println("ERROR CRITICAL");//Aucune frame à pin 0
-				return null;
-			}
-			Frame framecible = null;;
-			int tempsreference = 9999999;
-			for(Frame frame2 : ListeDesFramesAPin0) {
-				if(tempsreference<frame2.getTime()) {//Parmis les pages à 0 on prends la plus ancienne LRU
-					tempsreference=frame2.getTime();
-					framecible=frame2;
-				}	
-			}
-			PageId pageADelete = ListeDesPages.get(ListeDesFrames.lastIndexOf(framecible));
-			FreePage(pageADelete,true);
-			ListeDesPages.set(ListeDesFrames.lastIndexOf(framecible), pageid);
-			int emplacement = ListeDesPages.lastIndexOf(pageid);
-			
-			
-			return ListeDesFrames.get(emplacement).getBuff();
-			
-			}
-		}
-		return null;//si pblm
-		
-		
-		
 	}
-	public void FreePage(PageId pageid, boolean valdirty) {
-			Frame tmpFrame = ListeDesFrames.get(ListeDesPages.lastIndexOf(pageid));
-			tmpFrame.setPin_count(tmpFrame.getPin_count()-1);
-			tmpFrame.setDirty(valdirty);
-			tmpFrame.setTime(time);
-	}
-	public void FlushAll() {
-		for(Frame frame : ListeDesFrames) {
-			if(frame.isDirty()) {
-				FreePage(ListeDesPages.get(ListeDesFrames.lastIndexOf(frame)), true);
-				frame.setDirty(false);
-			}
-		}
+public ByteBuffer getPage(PageId PID) throws IOException {
 		
-	}
+		ByteBuffer b = ByteBuffer.allocate(DBParams.pageSize);
+		int cases = DBParams.frameCount;
+		int j = 0;
+		DiskManager disk = DiskManager.getLeDiskManager();
+		for (int i = 0; i < cases; i++) {
+			if (listeDesFrames[i].getPID() != null) {
+				if (listeDesFrames[i].getPID() == PID) {
+					listeDesFrames[i].setPin_count(listeDesFrames[i].getPin_count() + 1);
 
+					return listeDesFrames[i].getBuff();
+				}
+			}
+		}
+		for (int i = 0; i < cases; i++) {
+			if (listeDesFrames[i].getPID() == null) {
+				listeDesFrames[i].setPin_count(listeDesFrames[i].getPin_count() + 1);
+				disk.ReadPage(PID);
+				listeDesFrames[i].setBuff(b);
+				listeDesFrames[i].setPID(PID);
+				return listeDesFrames[i].getBuff();
+			}
+		}
+		
+		int cpt = listeDesFrames[0].getTemps_free();
+		
+		
+		for (int i = 1; i < cases; i++) {
+			if (cpt > listeDesFrames[i].getTemps_free()) {
+				cpt = listeDesFrames[i].getTemps_free();
+				j=i;
+			}
+		}
+		if(listeDesFrames[j].getPin_count()==0 && listeDesFrames[j].getDirty()==0) {
+			listeDesFrames[j].setPin_count(listeDesFrames[j].getPin_count() + 1);
+			disk.ReadPage(PID);
+			listeDesFrames[j].setBuff(b);
+			return listeDesFrames[j].getBuff();
+
+		}
+		if(listeDesFrames[j].getPin_count()==0 && listeDesFrames[j].getDirty()!=0) {
+			disk.WritePage(listeDesFrames[j].getPID(), listeDesFrames[j].getBuff());
+			listeDesFrames[j].setPin_count(listeDesFrames[j].getPin_count() + 1);
+			disk.ReadPage(PID);
+			listeDesFrames[j].setBuff(b);
+			return listeDesFrames[j].getBuff();
+		}
+		return null;
+
+	}
+	public void FreePage(PageId PID, int valdirty) {
+		int cases = DBParams.frameCount;
+		for (int i = 0; i < cases; i++) {
+			if (listeDesFrames[i].getPID()==PID) {
+				listeDesFrames[i].setDirty(valdirty);
+				listeDesFrames[i].setPin_count(listeDesFrames[i].getPin_count()-1);
+				time++;
+				listeDesFrames[i].setTemps_free(time);
+			}
+		}
+	}
+	public void FlushAll() throws IOException {
+		int cases = DBParams.frameCount;
+		DiskManager disk = DiskManager.getLeDiskManager();
+		for (int i = 0; i < cases; i++) {
+			if(listeDesFrames[i].getDirty()!=0) {
+				disk.WritePage(listeDesFrames[i].getPID(), listeDesFrames[i].getBuff());
+				listeDesFrames[i].setDirty(0);
+				listeDesFrames[i].setPin_count(0);
+				listeDesFrames[i].setBuff(ByteBuffer.allocate(DBParams.pageSize));
+				listeDesFrames[i].setTemps_free(0);
+			}else {
+				listeDesFrames[i].setBuff(ByteBuffer.allocate(DBParams.pageSize));
+				listeDesFrames[i].setPin_count(0);
+				listeDesFrames[i].setTemps_free(0);
+			}
+		}
+	}
+	
+	
 }
--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Frame.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Frame.java
@@ -1,34 +1,53 @@
 import java.nio.ByteBuffer;
 
 public class Frame {
-	private static ByteBuffer buff;
-	private int pin_count=0;
-	private boolean dirty=false;
-	private int time;
+
+	private PageId PID;
+	private ByteBuffer buff;
+	private int pin_count;
+	private int dirty;
+	private int temps_free;
+	
 	public Frame() {
-		ByteBuffer.allocate(DBParams.pageSize);	
+	
 	}
 	
+	public PageId getPID() {
+		return PID;
+	}
+	public void setPID(PageId pID) {
+		PID = pID;
+	}
 	public ByteBuffer getBuff() {
 		return buff;
+	}
+	public void setBuff(ByteBuffer buff) {
+		this.buff = buff;
 	}
 	public int getPin_count() {
 		return pin_count;
 	}
 	public void setPin_count(int pin_count) {
 		this.pin_count = pin_count;
+		
+	
 	}
-	public boolean isDirty() {
+	public int getDirty() {
 		return dirty;
 	}
-	public void setDirty(boolean dirty) {
+	public void setDirty(int dirty) {
 		this.dirty = dirty;
 	}
-	public int getTime() {
-		return time;
-	}
-	public void setTime(int time) {
-		this.time = time;
+
+
+
+	public int getTemps_free() {
+		return temps_free;
 	}
 
+	public void setTemps_free(int temps_free) {
+		this.temps_free = temps_free;
+	}
+
+	
 }
