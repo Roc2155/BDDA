4870d73020da39ee5ece1981aace6cb5cdf45979
Ouliviaa <oliviachen0815@gmail.com>
2022-10-14 22:11:40 +0200

    TestBufferManager

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/TestBufferManager.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/TestBufferManager.java
@@ -7,33 +7,39 @@ public class TestBufferManager {
 		PageId pid = new PageId(0, 1);
 		PageId pid2 = new PageId(0, 2);
 		PageId pid3 = new PageId(0, 3);
-		
+
 		try {
+			System.out.print("GET(0, 1) :  ");
 			BufferManager.getInstance().getPage(pid);
+			System.out.print("GET(0, 2) : ");
 			BufferManager.getInstance().getPage(pid2);
+			System.out.println("GET(0, 1) : ");
 			BufferManager.getInstance().getPage(pid);
-			BufferManager.getInstance().FreePage(pid2, 1);
-			BufferManager.getInstance().FreePage(pid, 0);
+			System.out.println("FREE((0, 2), 0): ");
+			BufferManager.getInstance().FreePage(pid2, 0);
+			System.out.println("FREE ((0, 1), 1)");
+			BufferManager.getInstance().FreePage(pid, 1);
+			System.out.print("FREE (0, 3) : ");
 			BufferManager.getInstance().getPage(pid3);
-			
+
 			Frame frame1 = BufferManager.getInstance().getFrame()[0];
 			if(pid.getFileIdx()!=frame1.getPID().getFileIdx()&&pid.getPageIdx()!=frame1.getPID().getPageIdx()){
 				System.out.println("Erreur");
 			}
-			
-			
+
+
 		} catch (IOException e) {
 			e.printStackTrace();
-		}		
-	}	
+		}
+	}
 	public static void main(String [] args) {
 		DBParams.DBPath = args[0];
-        DBParams.pageSize = 4;
-        DBParams.maxPagesPerFile = 4;
-        DBParams.frameCount = 2;
+    DBParams.pageSize = 4;
+    DBParams.maxPagesPerFile = 4;
+    DBParams.frameCount = 2;
 		BufferManager.getInstance().init();
 		TestBufferManager.TestBuffer();
 	}
-		
-	
+
+
 }
