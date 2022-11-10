e5d6ad23583ee2cd170c4fa8859521af9330318b
Roc2155 <107709060+Roc2155@users.noreply.github.com>
2022-11-10 21:34:03 +0100

    Update Main.java
    Ajout de la boucle de traitement des commandes.

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Main.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Main.java
@@ -7,6 +7,26 @@ public class Main
         DBParams.pageSize = 4096;
         DBParams.maxPagesPerFile = 4;
         DBParams.frameCount = 2;
-
+        
+        DBManager.getLeDBManager().Init();
+		Scanner sc=new Scanner(System.in);
+		String chaine;
+		while (true){
+			System.out.println("Veuillez choisir une commande :");
+			menu();
+			chaine=sc.nextLine();
+			if (chaine.equals("EXIT")){
+				DBManager.getLeDBManager().Finish();
+				break;
+			} else 
+				DBManager.getLeDBManager().ProcessCommand(chaine);
+		}
+		sc.close();
     }
+    private static void menu () {
+    	System.out.println("Entrez CREATE TABLE pour créer une table");
+    	//to do
+    	
+    }
+
 }
