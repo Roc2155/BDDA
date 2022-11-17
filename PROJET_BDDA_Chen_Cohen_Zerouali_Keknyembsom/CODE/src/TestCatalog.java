4ca6be56302c8b1e2c299dcdb27a44925d946984
Ouliviaa <oliviachen0815@gmail.com>
2022-11-17 13:04:53 +0100

    Modif

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/TestCatalog.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/TestCatalog.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 
 public class TestCatalog {
 	public static RelationInfo createRelationInfo(PageId page) {
-		
+
 		ColInfo col1 = new ColInfo("Nom","VARCHAR");
 		ColInfo col2 = new ColInfo("Prenom","VARCHAR");
 		ColInfo col3 = new ColInfo("Age","INTEGER");
-		ArrayList<ColInfo> listeCollones = new ArrayList<ColInfo>();
-		listeCollones.add(col1);
-		listeCollones.add(col2);
-		listeCollones.add(col3);
-		
-		RelationInfo rel1 = new RelationInfo("Etudiant", 3, listeCollones, page);
-		
+		ArrayList<ColInfo> listeColonnes = new ArrayList<ColInfo>();
+		listeColonnes.add(col1);
+		listeColonnes.add(col2);
+		listeColonnes.add(col3);
+
+		RelationInfo rel1 = new RelationInfo("Etudiant", 3, listeColonnes, page);
+
 		return rel1;
 	}
 
@@ -48,7 +48,7 @@ public class TestCatalog {
       Record r1 = createRecord(rel1);
       System.out.println("Info du record : " + r1.getRelInfo());
       System.out.println("Valeur du record : " + r1.getValues());
-      
+
       r1.getValues().add("Cohen");
       r1.getValues().add("Rahel");
       r1.getValues().add("20");
@@ -59,7 +59,7 @@ public class TestCatalog {
       ByteBuffer buffNom = ByteBuffer.wrap("Zerouali".getBytes());
       ByteBuffer buffPre = ByteBuffer.wrap("Faycal".getBytes());
       ByteBuffer buffNum = ByteBuffer.wrap("1".getBytes());
-      
+
       r1.writeToBuffer(buffNom, 1);
       r1.writeToBuffer(buffPre, 1+buffNom.capacity());
       r1.writeToBuffer(buffNum, 1+buffNom.capacity()+buffPre.capacity());
