5ae9135aaf9aa23b6a022f74f8ef00d83d9487b1
Roc2155 <107709060+Roc2155@users.noreply.github.com>
2022-12-01 13:00:21 +0100

    Ajout de commentaires dans la classe catalogue.

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Catalog.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Catalog.java
@@ -9,6 +9,8 @@ public class Catalog
     private List<RelationInfo> list = new ArrayList<>();
     private Catalog(){
     }
+    
+    //Singleton
     public static Catalog getCatalog(){
         if (catalog==null){
             catalog=new Catalog();
@@ -16,6 +18,7 @@ public class Catalog
         return catalog;
     }
 
+  //méthode qui lit les informations du fichier catalog.sv et remplit le Catalog avec ces informations.
     public void init() throws IOException {
         if (svCatalog.length() != 0) {
             InputStream is = new FileInputStream(svCatalog);
@@ -39,6 +42,8 @@ public class Catalog
             }
         }
     }
+    
+    //sauvegarde les informations du Catalog dans un fichier nommé catalog.sv
     public void finish() throws IOException {
         OutputStream os = new FileOutputStream(svCatalog);
         ObjectOutputStream oos = new ObjectOutputStream(os);
@@ -49,6 +54,7 @@ public class Catalog
         oos.close();
     }
 
+    //méthode qui prend en argument une RelationInfo et qui la rajoute à la liste 
     public void addRelationInfo(RelationInfo r) {
         if(!list.contains(r)) {
             list.add(r);
@@ -58,7 +64,7 @@ public class Catalog
             System.out.println("Relation existante dans la liste de relation");
         }
     }
-
+ //méthode qui prend en argument le nom d’une relation et retourne la RelationInfo associée.
     public RelationInfo getRelationInfo(String nom) {
         for (RelationInfo relationInfo : list) {
             if (relationInfo.getNom().equals(nom))
@@ -68,6 +74,7 @@ public class Catalog
         return null;
     }
 
+    //getteurs
     public int getNbreRelation()
     {
         return list.size();
