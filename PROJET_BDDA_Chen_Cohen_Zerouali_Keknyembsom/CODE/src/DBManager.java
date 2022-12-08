a393fe620303d022a85d99e4736e7e94c1f86ae4
Roc2155 <107709060+Roc2155@users.noreply.github.com>
2022-12-08 13:55:33 +0100

    last DBManager

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/DBManager.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/DBManager.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 //Point d'entrée du SGBD
 public class DBManager {
     private static DBManager leDBManager; 
-	
+
 	public static DBManager getLeDBManager() {
 
 		if(leDBManager == null) {
@@ -14,7 +14,7 @@ public class DBManager {
 		}
 		return leDBManager;
 	}
-	
+
     public void Init(){
     	//Appel à la méthode init de catalog + gestion des exceptions
         try {
@@ -26,12 +26,12 @@ public class DBManager {
         	System.out.println(" Erreur E/S ");
 			e.printStackTrace();
         }
-   
+
         //Appel de la méthode init de BufferManager
         BufferManager.getInstance().init();
-       
+
     }
-   
+
     public void Finish() {
     	//Appel de la méthode finish de DiskManager
     	try {
@@ -41,48 +41,47 @@ public class DBManager {
     	}
 
     	//Appel de la méthode FlushAll de BufferManager
-    	
+
     	try {
         	BufferManager.getInstance().FlushAll();
     	}catch (IOException e) {
     		e.printStackTrace();
     	}	
     }
-    
-    
-    public void processCommand(String  commande){
-        String cmd[] = commande.split(" ");
 
-        try{
-            switch(cmd[0]){
-                case "CREATE":
-                    CreateTableCommand c = new CreateTableCommand(commande);
-                    c.execute();
+
+    //A compléter + ajouter la gestion des exceptions
+    public void ProcessCommand(String ch){
+        StringTokenizer st =new StringTokenizer(ch);
+        String deb=st.nextToken();
+        switch (deb) {
+            case "CREATE":
+                CreateTableCommand createTable = new CreateTableCommand(ch);
+                createTable.Execute();
                 break;
-                case "DROPDB":
-                    DropDb.execute();
+            case "DROPDB":
+                DROPDBCommand drop=new DROPDBCommand(ch);
+                drop.Execute(); 
                 break;
-                case "INSERT":
-                    if(commande.contains("FILECONTENTS")){
-                        InsertionFile insert= new InsertionFile(commande);
-                        insert.insererFichier();
-                    }else{
-                        Insert i = new Insert(commande);
-                        i.execute();
-                    }
+            case "INSERT":
+                InsertCommand insert=new InsertCommand(ch);
+                insert.Execute();
                 break;
-                case "SELECT":
-                    Select s = new Select(commande);
-                    s.execute();
-                break;
-            }
-        }catch(IOException e){
-            e.printStackTrace();
+            case "SELECT":
+            	 SelectCommande select= new SelectCommand(ch);
+            	 select.Execute();
+
+            case "DELETE":
+            	DeleteCommand delete = new DeleteCommand(ch);
+            	delete.Execute();
+            	break;
+
+            default:
+            	System.out.println("Veuillez entrer une commande valide.");
         }
     }
 
-    
+
   } 
-    
-    
-    
+
+
