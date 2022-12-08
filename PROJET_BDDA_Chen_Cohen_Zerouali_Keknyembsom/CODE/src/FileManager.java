0ee1e57f94fcca6efff52e27d93ffc01b83d45ae
Ouliviaa <oliviachen0815@gmail.com>
2022-12-08 13:28:50 +0100

    createNewHeaderPage()

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/FileManager.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/FileManager.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class FileManager {
 
     private static FileManager instance = null;
-    
+
 
     public static final FileManager getInstance()
 	{
@@ -18,42 +18,54 @@ public class FileManager {
 			instance = new FileManager();
 		return instance;
 	}
-    public PageId createNewHeaderPage() throws IOException {
-        
-      }
-    public PageId getFreeDataPageId(RelationInfo relInfo, int sizeRecord) throws FileNotFoundException, IOException, EmptyStackException 
+
+  public PageId createNewHeaderPage() throws IOException {
+    DiskManager dm = DiskManager.getLeDiskManager();
+    BufferManager  bm = BufferManager.getInstance();
+    bm.init();
+
+    PageId headerPageId = dm.allocPage();
+    System.out.println("HeaderPage alloué : " + headerPageId.toString());
+
+    ByteBuffer buf = bm.getPage(headerPageId);
+    buf.putInt(0, 0);
+
+    bm.FreePage(headerPageId, 1);
+    return headerPageId;
+  }
+    public PageId getFreeDataPageId(RelationInfo relInfo, int sizeRecord) throws FileNotFoundException, IOException, EmptyStackException
 	{
-    	
+
     }
     public PageId addDataPage(RelationInfo relInfo) throws IOException {
-        
-        
+
+
       }
-    
+
     public ArrayList<PageId> getAllDataPages(RelationInfo relInfo) throws IOException {
-    	
+
 	  }
 
   public RecordId writeRecordToDataPage(Record record, PageId pageId) throws IOException {
-	  
+
 	  }
 
-  
+
 
   public ArrayList<Record> getRecordsInDataPage(RelationInfo relInfo, PageId pageId) throws IOException {
-	    
+
 	  }
   public RecordId insertRecordIntoRelation(Record record) throws IOException {
 
 	    return writeRecordToDataPage(record,getFreeDataPageId(record.getRelInfo(), record.recordSizeFromValues()));
 	  }
   public ArrayList<Record> getAllRecords(RelationInfo relInfo) throws IOException {
-	    
-	  
-	    
+
+
+
 	  }
-  
 
 
-  
+
+
 }
