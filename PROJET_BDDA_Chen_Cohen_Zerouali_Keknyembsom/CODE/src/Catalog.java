8fbca1310fabec887d2cafe7668d14d376ca5244
Ouliviaa <oliviachen0815@gmail.com>
2022-12-10 17:23:55 +0100

    RelationInfo

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Catalog.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/Catalog.java
@@ -6,7 +6,7 @@ public class Catalog
 {
     private static Catalog catalog;
     private File svCatalog = new File(DBParams.DBPath+File.separator+"catalog.sv");
-    private List<RelationInfo> list = new ArrayList<>();
+    private List<RelationInfo> listRelatInfo = new ArrayList<>();
     private Catalog(){
     }
 
@@ -23,14 +23,14 @@ public class Catalog
         if (svCatalog.length() != 0) {
             InputStream is = new FileInputStream(svCatalog);
             ObjectInputStream ois = new ObjectInputStream(is);
-            list.clear();
+            listRelatInfo.clear();
             boolean restaL = false;
             try {
                 restaL = true;
                 while (restaL) {
                     try {
                         RelationInfo rf = (RelationInfo) ois.readObject();
-                        list.add(rf);
+                        listRelatInfo.add(rf);
                         restaL = true;
                     } catch (Exception e) {
                         restaL = false;
@@ -43,14 +43,14 @@ public class Catalog
         }
     }
     public void reset(){
-        list.clear();
+        listRelatInfo.clear();
     }
 
     //sauvegarde les informations du Catalog dans un fichier nommé catalog.sv
     public void finish() throws IOException {
         OutputStream os = new FileOutputStream(svCatalog);
         ObjectOutputStream oos = new ObjectOutputStream(os);
-        for (RelationInfo relationInfo : list)
+        for (RelationInfo relationInfo : listRelatInfo)
         {
             oos.writeObject(relationInfo);
         }
@@ -58,12 +58,28 @@ public class Catalog
     }
 
     //méthode qui prend en argument une RelationInfo et qui la rajoute à la liste
-    public void addRelationInfo(RelationInfo r) {
-
+    public void addRelationInfo(RelationInfo relationInfo) {
+      if(!listRelatInfo.contains(relationInfo)) {
+            listRelatInfo.add(relationInfo);
+            System.out.println("Ajout de la relation dans la liste avec succès!");
+        }
+        else {
+            System.out.println("Relation existante dans la liste de relation");
+        }
     }
+
  //méthode qui prend en argument le nom d’une relation et retourne la RelationInfo associée.
     public RelationInfo getRelationInfo(String nom) {
-        return null;
+      RelationInfo r = null;
+        for(RelationInfo relationInfo : listRelatInfo) {
+          if(relationInfo.getNom().equals(nom)) {
+            r = relationInfo;
+          }
+          else {
+            System.out.println("La relation " + nom + " n'existe pas !!");
+          }
+        }
+        return r;
     }
 
     //getteurs
