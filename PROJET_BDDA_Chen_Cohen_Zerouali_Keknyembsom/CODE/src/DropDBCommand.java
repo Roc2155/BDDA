905d27e6686573c334aaa216713ce4157452650f
Ouliviaa <oliviachen0815@gmail.com>
2022-12-14 15:38:16 +0100

    DROPDB ok

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/DropDBCommand.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/DropDBCommand.java
@@ -32,26 +32,20 @@ public class DropDBCommand{
     	File[] liste = repDB.listFiles();
       System.out.println("Liste des fichiers présents dans le répertoire : ");
     	for(int i=0; i<liste.length; i++) {
-    		System.out.println(liste[i].getName());
+    		System.out.println("\t"+liste[i].getName());
     	}
     	if (liste !=null) {
     		 for(File item : liste){
-    		        if(item.isFile())
-    		        {
-    		        	//PAS SUR QUON PUISSE SUPPRIMER LES GITIGNORE ...QUI SONT DANS CE DOSSIER
-    		        	//TODO
-    		        	//item.delete();
-
-    		          //affichage des noms des fichiers/r�pertoires :
-    		          //System.out.format("Nom du fichier: %s%n", item.getName());
+           System.out.println("\n*****************SUPPRESSION DES FICHIERS************************");
+    		        if(item.isFile()) {
+                  item.delete();
+    		        	System.out.format("Nom du fichier: ", item.getName());
     		        }
     		        else if(item.isDirectory())
     		        {
-    		        	//appel recursif
+                  //appel recursif
     		        	deleteDB(item.getPath());
-
-
-    		          //System.out.format("Nom du r�pertoire: %s%n", item.getName());
+    		        	System.out.format("Nom du répertoire: ", item.getName());
 
     		      }
     		      //source : https://waytolearnx.com/2020/03/lister-le-contenu-dun-dossier-en-java.html
