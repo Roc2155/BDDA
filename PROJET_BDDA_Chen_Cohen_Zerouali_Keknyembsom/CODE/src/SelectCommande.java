6a7566c19af34553a8ae14e93d5e45113263f62c
Ouliviaa <oliviachen0815@gmail.com>
2022-12-14 17:59:08 +0100

    selectCommande début

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/SelectCommande.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/SelectCommande.java
@@ -1,10 +1,44 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class SelectCommande {
-	
+	private ArrayList<Record> resultatRecord;
+	private final static int MAX_CRITERE = 20;
+	private String nomRelation;
+	private StringBuffer conditions;
+	private StringTokenizer recordTokens;
+
 	public SelectCommande(String ch) {
-	
+		resultatRecord = new ArrayList<Record>();
+
+		//Parsing de la chaine saisie par l'utilisateur
+		StringTokenizer tokenizer = new StringTokenizer(ch, " *");
+		tokenizer.nextToken(); //SELECT
+		tokenizer.nextToken(); //FROM
+		nomRelation = tokenizer.nextToken();
+		if(tokenizer.hasMoreElements()) {
+			tokenizer.nextToken(); //WHERE
+			while(tokenizer.hasMoreElements()) {
+				String token = tokenizer.nextToken();
+				conditions = new StringBuffer();
+				if(!token.equals("AND")) {
+					conditions.append(token+" ");
+				}
+			}
+		}
+		System.out.println(conditions.toString());
 	}
-	
+
 	public void Execute() {
-		
+
+	}
+
+	public void recordsSelected() {
+		recordTokens = new StringTokenizer(conditions.toString(), " ");
+		while(recordTokens.hasMoreElements()) {
+
+		}
 	}
 }
