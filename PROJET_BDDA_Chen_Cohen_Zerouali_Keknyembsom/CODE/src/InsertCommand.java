e04328ef655109d0fdec69151d8ac67ad3ed32b4
Ouliviaa <oliviachen0815@gmail.com>
2022-12-14 16:34:53 +0100

    Fin InsertCommand

--- PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/InsertCommand.java
+++ PROJET_BDDA_Chen_Cohen_Zerouali_Keknyembsom/CODE/src/InsertCommand.java
@@ -15,10 +15,26 @@ public class InsertCommand {
 		tokenizer.nextToken(); //INTO
 		nomRelationInfo = tokenizer.nextToken();
 		//(val1,val2,...,valn)
-
+		StringTokenizer tokensValues = new StringTokenizer(tokenizer.nextToken(), ",)(");
+		System.out.println(tokensValues.countTokens());
+		while(tokensValues.hasMoreElements()) {
+			valeurs.add(tokensValues.nextToken());
+		}
+		System.out.println("************Valeurs insérées**************");
+		for(int i=0; i<valeurs.size(); i++) {
+			System.out.print("\t"+valeurs.get(i)+" ");
+		}
+		System.out.println("");
 	}
 
 	public void Execute() {
-
+		RelationInfo rel = Catalog.getCatalog().getRelationInfo(nomRelationInfo);
+		Record record = new Record(rel, valeurs);
+		try {
+			FileManager.getInstance().insertRecordIntoRelation(record);
+			valeurs.clear();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
